ca6c084764b51000ff1d0ba9aad28ce89f6e27cb
Harshit Mittal <harshitmittal@Harshits-MacBook-Pro.local>
2025-12-09 16:53:46 +0530

    frontend changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Navbar from "./components/Navbarhorizontal";
 import Navbarvertical from "./components/Navbarvertical";
 
-console.log("DEBUG:", { Login, Signup, Dashboard, Navbar , Navbarvertical});
+console.log("DEBUG:", { Home, Login, Signup, Dashboard, Navbar , Navbarvertical});
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
